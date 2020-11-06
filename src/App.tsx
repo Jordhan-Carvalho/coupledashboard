a83987b2cbf82cd063f05bda3cb1665384aac0ce
Jordhan <jordhan.rdz@gmail.com>
2020-11-06 09:49:41 -0300

    Initial Setup + background

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
-function App() {
+import DashboardPage from "./pages/DashboardPage";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact component={DashboardPage} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+const GlobalStyle = createGlobalStyle`
+:root {
+  --fontMontserrat: 'Montserrat', sans-serif;
+}
+
+body {
+  font-family: var(----fontMontserrat);
+}
+
+#root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+background: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E"), linear-gradient(116.82deg, #E9F8FF 0%, rgba(202, 240, 248, 0.354167) 48.96%, rgba(202, 240, 248, 0) 100%);
+background-blend-mode: overlay, normal;
+
+}
+
+`;
